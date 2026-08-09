6af1d9edf48df726833f59bf90056b4292e8060e
2061969365 <2061969365@qq.com>
2026-08-09 11:07:33 +0800

    fix: add material-icons-core dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
 
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.core:core-ktx:1.18.0")
