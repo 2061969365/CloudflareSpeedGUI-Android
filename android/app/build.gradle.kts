521ed669a838df86dd5942bfb318cb08846d6456
2061969365 <2061969365@qq.com>
2026-08-10 15:51:57 +0800

    chore: bump version to 1.2.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.cfst.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2.1"
+        versionCode = 6
+        versionName = "1.2.3"
     }
 
     sourceSets["main"].kotlin.srcDirs("../../engine/src/main/kotlin")
